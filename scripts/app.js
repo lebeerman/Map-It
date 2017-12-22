8f96b7efb34181c2efece49a23c29170637b0811
lebeerman <omnidbeerman@gmail.com>
2017-12-21 22:58:50 -0700

    firebase deploy. building app.js. index. main.css. leaaflet map + user input form.html.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
 const url = "https://p2-webapp-server.herokuapp.com/"
 
-
-
 document.querySelector('body').innerHTML += 'JS LOADED!';
+
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibGViZWVybWFuIiwiYSI6ImNqYmd1bnUxZzNjZG0ycmxscHozaDFja3MifQ.3RRZRD3bBsMfgNdW5-J8cQ'
+}).addTo(mymap);
 
 
 
 fetch(url)
 .then(response => response.json())
 .then(response => {
-  document.querySelector('body').innerHTML += response;
+  console.dir(response);
 });
